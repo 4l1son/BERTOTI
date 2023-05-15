14b86525f91673ccd00ab99fdd5cb7d05cffe8fe
Alisson dos Santos Pereira <111581261+4l1son@users.noreply.github.com>
2023-05-15 20:09:44 -0300

    Add files via upload

---
+++ ENGENHARIA DE SOFTWARE/Código java/pteste.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class pteste {
+    @Test
+    public void EmpresaTeste(){
+        Empresa emp = new Empresa();
+        emp.
+    }
+}
